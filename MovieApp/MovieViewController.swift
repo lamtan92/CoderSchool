fbc16fa04dd350b4064891e9b000b1c82cc2fa29
Tran Tan Lam <trankind@gmail.com>
2016-07-08 23:15:22 +0700

    Optimize requirement code

--- MovieApp/MovieViewController.swift
+++ MovieApp/MovieViewController.swift
@@ -11,14 +11,16 @@ import AFNetworking
 import MBProgressHUD
 import ChameleonFramework
 
-class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+class MovieViewController: UIViewController {
+  
+    @IBOutlet weak var networkError: UILabel!
+    @IBOutlet weak var movieTable: UITableView!
+    
     var movies = [NSDictionary]()
     var baseUrl = "http://image.tmdb.org/t/p/w45"
     var posterUrl:NSURL?
     
-    @IBOutlet weak var networkError: UILabel!
-    @IBOutlet weak var movieTable: UITableView!
+    let refreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,41 +30,12 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         movieTable.dataSource = self
         movieTable.delegate = self
         
-        //  Get data from movie API
-        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
-        let request = NSURLRequest(
-            URL: url!,
-            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
-            timeoutInterval: 10)
-        
-        let session = NSURLSession(
-            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-            delegate: nil,
-            delegateQueue: NSOperationQueue.mainQueue()
-        )
         
         //  Show progress
         MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
-                                                                     completionHandler: { (dataOrNil, response, error) in
-                                                                        if let data = dataOrNil {
-                                                                            if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-                                                                                data, options:[]) as? NSDictionary {
-                                                                                print("response: \(responseDictionary)")
-                                                                                self.movies = responseDictionary["results"] as! [NSDictionary]
-                                                                                self.movieTable.reloadData()
-                                                                                MBProgressHUD.hideHUDForView(self.view, animated: true)
-                                                                            }
-                                                                        } else {
-                                                                            self.networkError.hidden = false
-                                                                            MBProgressHUD.hideHUDForView(self.view, animated: true)
-                                                                        }
-        })
-        task.resume()
+        loadData()
         
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), forControlEvents: UIControlEvents.ValueChanged)
+        refreshControl.addTarget(self, action: #selector(loadData), forControlEvents: UIControlEvents.ValueChanged)
         movieTable.insertSubview(refreshControl, atIndex: 0)
     }
 
@@ -76,27 +49,9 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
-    }
-    
-    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
-    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as! MovieTableViewCell!
-
-        cell.titleLabel?.text = movies[indexPath.row]["title"] as? String
-        cell.overviewLabel?.text = movies[indexPath.row]["overview"] as? String
-
-        posterUrl = NSURL(string: baseUrl + (movies[indexPath.row]["poster_path"] as? String)!)
-        cell.posterImage.setImageWithURL(posterUrl!)
-
-        return cell
-    }
-    
-    //  MARK: - RefreshControl
-    func refreshControlAction(refreshControl: UIRefreshControl) {
+  
+    //  MARK: -Load data
+    func loadData(){
         
         //  Get data from movie API
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
@@ -120,20 +75,20 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
                                                                                 print("response: \(responseDictionary)")
                                                                                 self.movies = responseDictionary["results"] as! [NSDictionary]
                                                                                 self.movieTable.reloadData()
-                                                                                refreshControl.endRefreshing()
+                                                                                self.refreshControl.endRefreshing()
+                                                                                MBProgressHUD.hideHUDForView(self.view, animated: true)
                                                                             }
                                                                         } else {
                                                                             self.networkError.hidden = false
-                                                                            refreshControl.endRefreshing()
+                                                                            MBProgressHUD.hideHUDForView(self.view, animated: true)
                                                                         }
         })
         
         task.resume()
+
     }
     
-    
-    
-    // MARK: - Navigation
+    //  MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -148,6 +103,31 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         detailViewController.movie = movie
     }
-    
+}
 
+extension MovieViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as! MovieTableViewCell!
+        
+        cell.titleLabel?.text = movies[indexPath.row]["title"] as? String
+        cell.overviewLabel?.text = movies[indexPath.row]["overview"] as? String
+        
+        posterUrl = NSURL(string: baseUrl + (movies[indexPath.row]["poster_path"] as? String)!)
+        cell.posterImage.setImageWithURL(posterUrl!)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //  Remove gray selection effect
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
